47941278a29fff02e897a03e711cd2d2906ff834
Jakub Rosa <jakub.rosa03@gmail.com>
2026-06-10 18:52:25 +0200

    fix: move necessary variables for DoomDataset to work

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,7 +1,39 @@
 from torch.utils.data import Dataset, DataLoader
+import os
+import json
+import numpy as np
+
 
 CONTEXT = 512
 STRIDE  = 256
+MAX_TRANSPOSE = 6
+PROC_DIR = '../data/processed/'
+
+with open(os.path.join(PROC_DIR, 'vocab.json')) as f:
+    token2id = json.load(f)
+id2token = {y: x for x,y in token2id.items()}
+VOCAB_SIZE = len(token2id)
+
+id2pitch = {}
+for tok, i in token2id.items():
+    if tok.startswith('NOTE_ON_') or tok.startswith('NOTE_OFF_'):
+        id2pitch[i] = int(tok.split('_')[-1])
+noteon_ids = { int(tok.split('_')[-1]): i for tok, i in token2id.items() if tok.startswith('NOTE_ON') }
+noteoff_ids = { int(tok.split('_')[-1]): i for tok, i in token2id.items() if tok.startswith('NOTE_OFF') }
+
+SHIFT_MAPS = {}
+for off in range(-MAX_TRANSPOSE, MAX_TRANSPOSE+1):
+    m = np.arange(VOCAB_SIZE)
+    for pitch, idx in noteon_ids.items():
+        np_ = pitch + off
+        if 0 <= np_ <= 127:
+            m[idx] = noteon_ids[np_]
+    for pitch, idx in noteoff_ids.items():
+        np_ = pitch + off
+        if 0 <= np_ <= 127:
+            m[idx] = noteoff_ids[np_]
+    SHIFT_MAPS[off] = m
+
 class DoomDataset(Dataset):
     def __init__(self, sequences, context=CONTEXT, stride=STRIDE, augment=True):
         self.context=context
